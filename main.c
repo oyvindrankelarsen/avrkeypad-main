465d5a4e68ad433908c6f10d491f34042d458d2f
oyvindrankelarsen <larsen@larsen.tk>
2023-09-25 18:47:54 +0200

    rensat, funkar inte

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <util/delay.h>
 #include "uart.h"
 #include "keypad.h"
-
+// https://wokwi.com/projects/376753312998681601 min
 // WOKWI https://wokwi.com/projects/375118289884211201
 
 int main()
@@ -12,11 +12,12 @@ int main()
   init_serial();
   keypadInit();
 
-  char ch = ' ';
+  char ch = 'X';
   uint8_t count = 0, sum = 0;
   while (count < 4)
   {
     char now = keypadScan();
+    //printf("%d\n", now);
     if (ch != now && now != ' ')
     {
       printf("%d\n", now);
